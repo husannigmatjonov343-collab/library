687e7442d48bd9e8cb01965aab77f59452a06384
husan <husannigmatjonov343@gmail.com>
2026-06-22 17:50:43 +0500

    Fix circular import and update vercel paths

--- library_site/wsgi.py
+++ library_site/wsgi.py
@@ -1,19 +1,9 @@
-"""
-WSGI config for library_site project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-from library_site.wsgi import application
-
-app = application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_site.settings')
 
 application = get_wsgi_application()
+
+# Vercel uchun asosiy kirish nuqtasi
+app = application
